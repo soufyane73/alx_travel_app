a6709790a02f5926ffabf3a77baaae7e0aaa1e00
soufyane73 <82409191+soufyane73@users.noreply.github.com>
2025-07-06 21:44:42 +0100

    Add all project files

---
+++ alx_travel_app/alx_travel_app/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+# This file is part of the alx_travel_app project.
+
+# Installed pymysql as MySQLdb/mysqlclient
+# This is a workaround for the MySQLdb/mysqlclient issue with Django.
+# It allows the use of pymysql as a drop-in replacement for MySQLdb.
+#
+
+# import pymysql
+# pymysql.install_as_MySQLdb()
